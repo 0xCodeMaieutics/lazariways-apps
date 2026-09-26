3895369cc79e3ef2bdb6b79aae419fb69e8b8285
0xCodeMaieutics <gio.shara12345@gmail.com>
2026-09-26 16:02:00 +0200

    fix(personalgeorgien): data scroll bnehaviour warning

--- apps/personalgeorgien/app/layout.tsx
+++ apps/personalgeorgien/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="de" className="antialiased">
+    <html lang="de" className="antialiased" data-scroll-behavior="smooth">
       <body className={manrope.className}>{children}</body>
     </html>
   )
